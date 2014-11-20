1baf8c804f48d52f381a7658340c93e25c1c723d
codyroot <smithyyy@myopera.com>
2014-11-20 17:18:05 +0100

    struct changes

--- 2_MultiVision/public/app/account/mvNavBarLoginCtrl.js
+++ 2_MultiVision/public/app/account/mvNavBarLoginCtrl.js
@@ -2,7 +2,7 @@
  * Login Controller
  */
 angular.module('app').controller('mvNavBarLoginCtrl', function ($scope) {
-    $scope.sign = function (/*username, password*/) {
+    $scope.signin = function (/*username, password*/) {
         console.log("Yo Iam here");
     };
 });
